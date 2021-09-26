fc7ea8a464223ca68e4fe8d19f81b8a6702c9b5e
Brian Yofre <brian.yofre7@gmail.com>
2021-09-26 18:45:53 -0500

    Programming Assignment 2 done!

--- cachematrix.R
+++ cachematrix.R
@@ -2,22 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##Set, get and save data 
 makeCacheMatrix <- function(x = matrix()) {
   inve<-NULL
   set<-function(y){
     x<<-y
     inve<<-NULL
   }
-  get<- function() {x} #get matrix x
+  #get matrix x
+  get<- function() {x} 
   setInverse<- function(inverse){inve<<-inverse}
-  getInverse<- function(){inve} #inverse of matrix
+  #inverse of matrix
+  getInverse<- function(){inve} 
   list(set=set,get = get,setInverse= setInverse, getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+## Function to get the cache data
 cacheSolve <- function(x, ...) {
   inve<-x$getInverse()
   if(!is.null(inve)){
@@ -25,7 +27,9 @@ cacheSolve <- function(x, ...) {
     return(inve)
   }
   data <- x$get()
-  inve<- solve(data,...)    #calculate inverse
+  #calculate inverse
+  inve<- solve(data,...)    
   x$setInverse(inve)
-  inve ## Return a matrix that is the inverse of 'x'
+  inve
+  ## Return a matrix that is the inverse of 'x'
 }
